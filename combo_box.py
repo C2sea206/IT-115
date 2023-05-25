24224db5676976d39e9f7d1469ddfe4299f34478
alex <abravov@wgu.edu>
2023-05-25 10:25:37 -0700

    finished combobox

--- combo_box.py
+++ combo_box.py
@@ -12,3 +12,16 @@ def on_select(event):
 #root window 
 root = tk.Tk()
 root.title("Combobox Example")
+
+#array of items
+items = ["item1", "item2", "item3", "item4", "item5"]
+
+#tie array with combo box
+combo_box = ttk.Combobox(root, values=items)
+combo_box.bind("<<ComboboxSelected>>", on_select)
+
+combo_box.pack()
+
+root.mainloop()
+
+
